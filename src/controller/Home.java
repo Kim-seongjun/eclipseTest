7c2a9fdf912b6086d6d77171953c2f7751582a17
Kim-seongjun <01090524238@naver.com>
2020-01-07 18:29:50 +0900

    테스트

--- src/controller/Home.java
+++ src/controller/Home.java
@@ -15,7 +15,7 @@ import service.MemberMM;
  */
 
 @WebServlet("/access,/petsittersearch,/joinfrm")
-
+//깃데스크탑 테스트입니다.
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
